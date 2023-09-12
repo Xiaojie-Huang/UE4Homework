7862c02afd84363d49cdae33bd9a71c3b835bd39
Xiaojie-Huang <1961895405@qq.com>
2023-09-12 14:04:54 +0800

    First Commit

---
+++ Source/HomeWork/HomeWorkGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HomeWorkGameMode.h"
+#include "HomeWorkHUD.h"
+#include "HomeWorkCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AHomeWorkGameMode::AHomeWorkGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AHomeWorkHUD::StaticClass();
+}
